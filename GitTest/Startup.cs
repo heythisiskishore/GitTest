5ec79a14b4b4d2446678aedb1bc702317165c74d
Sai Kishore <heythisiskishore@gmail.com>
2020-05-11 10:43:23 +0530

    first change committed..

--- GitTest/Startup.cs
+++ GitTest/Startup.cs
@@ -27,7 +27,7 @@ namespace GitTest
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello Kishore! First Change");
             });
         }
     }
